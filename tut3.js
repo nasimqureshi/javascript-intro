a117a4f359bb54cfbfc5b35ae95a072c6d2f78f1
nasimqureshi <qureshi.nasim@yahoo.com>
2022-12-25 16:32:28 -0600

    my commit

---
+++ tut3.js
@@ -0,0 +1,16 @@
+console.log('tut3')
+
+// variables in js
+// var, let, const
+
+var name = 'Nasim'
+var channel = 'Qamar'
+var marks = 34
+ 
+console.log(name, channel, marks)
+
+var city = 'karachi';
+// const name = 'Nasim Qureshi';
+// const fruit = 'orange';
+console.log(city )
+console.log(marks )
