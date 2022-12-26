eb804cb5cebdc9e757e0abbe67ef2f4b2c10d848
nasimqureshi <qureshi.nasim@yahoo.com>
2022-12-25 18:30:31 -0600

    commit

--- tut4.js
+++ tut4.js
@@ -1,5 +1,16 @@
 // Primitive data types
 // String
-var name = 'nasim'
+let name = 'nasim'
 console.log("My string is " + name);
 console.log("Data type is "  + (typeof name));
+
+let nullVar = null;
+console.log("Data type is " + typeof nullVar);
+
+let StMarks = {
+    nasim: 90,
+    khalid: 75,
+    shahid: 68, 
+    qamar: 46
+}
+console.log(StMarks)
---
+++ tut6.js
@@ -0,0 +1,7 @@
+console.log('We are at tut 6')
+
+const name = 'Nasim Qureshi';
+const greeting = 'Good Morning';
+
+
+console.log(greeting   + ' ' + name);
